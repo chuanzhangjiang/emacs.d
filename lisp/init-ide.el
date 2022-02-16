92fa9d48a473930113c288ac0d53af32dfdb167c
chuanzhangjiang <chuanzhangjiang@outlook.com>
2022-02-16 17:01:36 +0800

    改变scala-mode下的<C-x C-f>为projectile-find-file

--- lisp/init-ide.el
+++ lisp/init-ide.el
@@ -13,24 +13,28 @@
 
 (use-package lsp-mode
   ;; Optional - enable lsp-mode automatically in scala files
-  :hook  (scala-mode . lsp)
-         (lsp-mode . lsp-lens-mode)
+  :hook
+    (scala-mode . lsp)
+    (lsp-mode . lsp-lens-mode)
   :config
-  ;; Uncomment following section if you would like to tune lsp-mode performance according to
-  ;; https://emacs-lsp.github.io/lsp-mode/page/performance/
-        (setq gc-cons-threshold 100000000) ;; 100mb
-        (setq read-process-output-max (* 1024 1024)) ;; 1mb
-        (setq lsp-idle-delay 0.500)
-        (setq lsp-log-io nil)
-        (setq lsp-completion-provider :capf)
-  ;; (setq lsp-prefer-flymake nil)
+    ;; Uncomment following section if you would like to tune lsp-mode performance according to
+    ;; https://emacs-lsp.github.io/lsp-mode/page/performance/
+    (setq gc-cons-threshold 100000000) ;; 100mb
+    (setq read-process-output-max (* 1024 1024)) ;; 1mb
+    (setq lsp-idle-delay 0.500)
+    (setq lsp-log-io nil)
+    (setq lsp-completion-provider :capf)
+    ;; (setq lsp-prefer-flymake nil)
   )
 
 ;; scala support
 ;; Enable scala-mode for highlighting, indentation and motion commands
 (use-package scala-mode
   :interpreter
-    ("scala" . scala-mode))
+    ("scala" . scala-mode)
+  :config
+  	(setq lsp-headerline-breadcrumb-enable nil)
+  	(define-key scala-mode-map (kbd "C-x C-f") 'projectile-find-file))
 
 ;; Enable sbt mode for executing sbt commands
 (use-package sbt-mode
